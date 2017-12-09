ea5242a3486602f36df6590bf94f73aed6c862eb
mishroom <mishall.afzal@gmail.com>
2017-12-08 17:35:04 -0800

    workign on chatter box events

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,16 +5,21 @@ var app = {
 };
 // console.log(this);
 app.init = function () {
-  
+  //event listeners here
+  //use .fetch to get messages
+  //create new instance for each message ?
+  this.fetch();
 };
 
+
+app.server = 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages';
 
 
 app.send = function (message) {
   
   $.ajax({
   // This is the url you should use to communicate with the parse API server.
-    url: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
+    url: this.server,
     type: 'POST',
     data: message,
     contentType: 'application/json',
@@ -30,7 +35,17 @@ app.send = function (message) {
 
 app.fetch = function () {
   $.ajax({
-    type: 'GET'
+    url: this.server,
+    type: 'GET',
+    data: message,
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
   });
 };
 
@@ -44,4 +59,15 @@ app.renderMessage = function () {
 
 app.renderRoom = function () {
   $('#roomSelect').append(message);
+};
+
+app.handleUsernameClick = function () {
+  // $('#main').find('.username').on('click', init);
+  // });
+};
+
+// app.handleUsernameClick = true; //LOLWUT THIS
+
+app.handleUsernameClick.restore = function () {
+  //LOLWUTTHIS#2
 };
