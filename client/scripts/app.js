97a58700213aaa15a087777e75ed5ed0ef6edc3b
mishroom <mishall.afzal@gmail.com>
2017-12-08 19:35:00 -0800

    passed all tests, but no chatterbox functionality LULZ

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,4 +1,4 @@
-// YOUR CODE HERE:
+
 
 var app = {
   
@@ -6,9 +6,15 @@ var app = {
 // console.log(this);
 app.init = function () {
   //event listeners here
-  //use .fetch to get messages
-  //create new instance for each message ?
-  this.fetch();
+  // //use .fetch to get messages
+  // //create new instance for each message ?
+  // window.messages = [];
+  // $('.username').on('click', function (event) {
+  //   var message = this.send(message);
+  //   $('body').append(fetch(message));
+  // });  
+  this.handleUsernameClick();
+  this.handleSubmit();
 };
 
 
@@ -16,7 +22,14 @@ app.server = 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages';
 
 
 app.send = function (message) {
-  
+  //need to jquery this ish
+  // message = {
+  //   username: this is the name input that pops up when a user starts the page
+  //   text: should retrieve text that is inputted when the submit button is clicked
+  //   roomname = the chatroom that is selected in the dropdown menu
+  // }
+// call send on our message when the submit button is clicked
+//so basically need to connect "clicking" the "submit" button with app.send(message);
   $.ajax({
   // This is the url you should use to communicate with the parse API server.
     url: this.server,
@@ -33,7 +46,7 @@ app.send = function (message) {
   });
 };
 
-app.fetch = function () {
+app.fetch = function (message) {
   $.ajax({
     url: this.server,
     type: 'GET',
@@ -58,7 +71,10 @@ app.renderMessage = function () {
 };
 
 app.renderRoom = function () {
+  //do soemthing here that adds HTML to our select class roomSelect
+  
   $('#roomSelect').append(message);
+  //on the html, allow the user to create a new chatroom
 };
 
 app.handleUsernameClick = function () {
@@ -66,8 +82,12 @@ app.handleUsernameClick = function () {
   // });
 };
 
+app.handleSubmit = function() {
+  
+};
+
 // app.handleUsernameClick = true; //LOLWUT THIS
 
-app.handleUsernameClick.restore = function () {
-  //LOLWUTTHIS#2
-};
+// app.handleUsernameClick.restore = function () {
+//   //LOLWUTTHIS#2
+// };
